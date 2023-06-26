38a1b41d55df34ae5c7ed9e08943a2f739a0564d
jvpaim <102495736+jvpaim@users.noreply.github.com>
2023-06-26 16:51:18 -0300

    tirei o erro do formulario

--- app.js
+++ app.js
@@ -1,38 +1,61 @@
-//variaveis do express e do handlebars
+// Variáveis do express e do handlebars
 var express = require('express');
 var { engine } = require('express-handlebars');
-var bp = require('body-parser');
 
-//App
+// App
 var app = express();
 
-//Body-parser
-app.use(bp.urlencoded({ extended: false }));
-app.use(bp.json());
+var bodyParser = require('body-parser');
 
-//Template
-//app.engine('handlebars', exphbs({defaultLayout : 'principal'}));
-app.engine('handlebars', engine({defaultLayout : 'principal'}));
+// Template
+app.engine('handlebars', engine({ defaultLayout: 'principal' }));
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-//Especificar arquivos estaticos 
+// Especificar arquivos estáticos 
 app.use(express.static(__dirname + '/publico'));
 
+// Array para armazenar os usuários cadastrados
+const users = [];
 
-//Rotas
+// Configurar o body-parser para lidar com solicitações JSON
+app.use(bodyParser.json());
+
+// Configurar o body-parser para lidar com solicitações URL-encoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Rotas
 app.get('/', function (req, res) {
     res.render('inicio');
 });
 
-app.get('/clientes', function (req, res) {
-    res.render('clientes');
-});
-
-app.get('/login', function (req, res) {
+// Rota para exibir o formulário de login
+app.get('/login', (req, res) => {
     res.render('login');
 });
 
+// Rota para lidar com o envio do formulário de login
+app.post('/login', (req, res) => {
+    const { name, email, password } = req.body;
 
-//Servidor
-app.listen(3000);
+    // Armazenar os dados do usuário cadastrado
+    users.push({ name, email, password });
+
+    // Redirecionar para a página de sucesso após o login bem-sucedido
+    res.redirect(`/suceso?name=${name}`);
+});
+
+// Rota para exibir a página de sucesso
+app.get('/suceso', (req, res) => {
+    const name = req.query.name;
+
+    res.render('suceso', { name });
+});
+
+// Rota para exibir os dados cadastrados
+app.get('/dados', (req, res) => {
+    res.render('dados', { users });
+});
+
+// Servidor
+app.listen(8080);
